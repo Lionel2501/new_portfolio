33528f796423a1cf3211b9cd230633707889ffc1
Lionel2501 <lionelcassar92@gmail.com>
2024-07-07 02:57:19 -0300

    index + _app updated

--- pages/_app.js
+++ pages/_app.js
@@ -4,12 +4,15 @@ import "@/styles/footer.css";
 import { Fragment, useEffect, useState } from "react";
 import "../public/css/backgroundDinamic.css"
 import "../public/css/carousel.css"
+import "../public/css/style.css"
+import "@/styles/swiper/css/style.css";
 import "../public/static/style/master.css"
 import "../src/backgroundDinamic"
 
+
 export default function App({ Component, pageProps }) {
-  const [loading, setLoading] = useState(true);
-  const [content, setContent] = useState(false);
+   const [loading, setLoading] = useState(true);
+ const [content, setContent] = useState(false);
   
   useEffect(() => {
     setTimeout(() => {
@@ -18,75 +21,20 @@ export default function App({ Component, pageProps }) {
     setTimeout(() => {
       setContent(true);
     }, 1000);
-  }, []);
+  }, []); 
 
   return (
     <>
       <div id="large-header" className="large-header">
         <canvas id="demo-canvas"></canvas>
-        <div className="contenedor">
-          <div className="contenedorItem">
-            <p className="main-title">Bienvenidos a mi Portfolio Digital</p>
-            <h1 className="title">Me llamo Lionel, soy programador full-stack</h1>
-          </div>
-          <div className="contenedorItem">
-            <h1 className="main-title">Tecnologias</h1>
-            <div className="container_carousel">
-                <div className="carousel_main">
-                  <div className="carousel__face"><span></span></div>
-                  <div className="carousel__face"><span></span></div>
-                  <div className="carousel__face"><span></span></div>
-                  <div className="carousel__face"><span></span></div>
-                  <div className="carousel__face"><span></span></div>
-                  <div className="carousel__face"><span></span></div>
-                  <div className="carousel__face"><span></span></div>
-                  <div className="carousel__face"><span></span></div>
-                  <div className="carousel__face"><span></span></div>
-                </div>
-            </div>
-          </div>
-        </div>
+          {/* <CarouselComponent /> */}
+          {/* <main className="main-left"> */}
+          <Fragment>
+              {loading && <Preloader />}
+              {content && <Component {...pageProps} />}
+          </Fragment> 
+        {/* </main> */}
       </div>
-      
-{/*
-      <main className="main-left">
-        <section
-          id="home"
-          className="home-banner-01 bg-cover bg-center bg-no-repeat"
-        >
-          <div className="container">
-            <div className="row full-screen align-items-center p-100px-tb">
-              <div className="col-md-12">
-                <div className="ht-text">
-                  <h6>Bienvenidos !</h6>
-                  <h1>Me llamó Lionel</h1>
-                  <h2>
-                    Soy apasionado 
-                  </h2>
-                  <p>
-                    The namics of how users interact with interactive elements
-                    within a user interface flow chart based on container
-                    proportion.
-                  </p>
-                  <div className="btn-bar go-to">
-                    <a className="m-btn m-btn-theme" href="#work">
-                      Mis Prácticas
-                    </a>
-                    <a className="m-btn m-btn-t-theme" href="#contactus">
-                      Contacto
-                    </a>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </section>
-      </main>
-*/}
-{/*        <Fragment>
-          {loading && <Preloader />}
-          {content && <Component {...pageProps} />}
-      </Fragment>  */}
     </>
   );
 }
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,9 +1,11 @@
 import About from "@/src/components/About";
 import Skills from "@/src/components/Skills";
 import Experience from "@/src/components/Experience";
+import Tecnologias from "@/src/components/Tecnologias";
+import Presentacion from "@/src/components/Presentacion";
+import Blog from "@/src/components/Blog";
+import CarouselComponent from "@/src/components/CarouselComponent";
 import { Fragment, useEffect, useState } from "react";
-
-import TypingAnimation from "@/src/components/TypingAnimation";
 import Footer from "@/src/layouts/Footer";
 import dynamic from "next/dynamic";
 
@@ -11,65 +13,24 @@ const Work = dynamic(() => import("@/src/components/Work"), {
   ssr: false,
 });
 
-const scrollToBottom = () => {
-  if (typeof window !== "undefined") { // Verificar si estamos en el lado del cliente
-    window.location.href = '#experience';
-  }
-}
-
 const Index1 = () => {
   const [toggle, setToggle] = useState(false);
 
   return (
     <Fragment>
-      <main className="main-left">
-        {/* Home Banner */}
-        <section
-          id="home"
-          className="home-banner-01 bg-cover bg-center bg-no-repeat"
-        >
-          <div className="container">
-            <div className="row full-screen align-items-center p-100px-tb">
-              <div className="col-md-12">
-                <div className="ht-text">
-                  <h6>Bienvenidos !</h6>
-                  <h1>Me llamó Lionel</h1>
-                  <h2>
-                    Soy apasionado <TypingAnimation />
-                  </h2>
-                  <p>
-                    The namics of how users interact with interactive elements
-                    within a user interface flow chart based on container
-                    proportion.
-                  </p>
-                  <div className="btn-bar go-to">
-                    <a className="m-btn m-btn-theme" href="#work">
-                      Mis Prácticas
-                    </a>
-                    <a className="m-btn m-btn-t-theme" href="#contactus">
-                      Contacto
-                    </a>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-{/*           <div className="go-to go-to-next">
-            <a href="#experience">
-              <span />
-            </a>
-          </div> */}
-        </section>
-        <Experience />
+      <CarouselComponent />
+{/*         <Presentacion />*/}
+        {/* <Tecnologias />  */}
+        {/* <Experience /> */}
         {/* <Skills /> */}
-        <About />
+        {/* <About /> */}
         {/* <Work /> */}
         {/* <Testiminails /> */}
         {/* <Blog /> */}
         {/* <Contact /> */}
-      </main>
-      <Footer />
+        {/* <Footer /> */}
     </Fragment>
   );
 };
+
 export default Index1;
